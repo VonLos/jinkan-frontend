c0dcc9c20ef2cb55cebd861baea000848935953c
VonLos <rylandgrounds@gmail.com>
2020-11-05 10:03:29 -0600

    login button created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div >
-      
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
